e5c10b66b82854a5252b6a642e02bb09ad3febee
Viggo Petersen <vig56@hotmail.com>
2022-02-18 10:16:56 +0100

    initial commit

---
+++ handin1/handin1/main.c
@@ -0,0 +1,18 @@
+/*
+ * handin1.c
+ *
+ * Created: 18/02/2022 10.14.48
+ * Author : viggo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
